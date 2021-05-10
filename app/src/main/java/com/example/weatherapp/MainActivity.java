698d0e4cb92bcd7baa324156bb90621ffad291b8
AlexSemenenko <aleksei.syemyenenko@gmail.com>
2021-05-10 19:14:32 +0300

    changed via new class

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.weatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,24 +18,25 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 import com.squareup.picasso.Picasso;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String API_KEY = "33fe9ce138c6c6607762c5cecd8f9eb7";
+    final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+
+    final long MIN_TIME = 10000;
+    final float MIN_DISTANCE = 1000;
+    final int REQUEST_CODE = 101;
+
+    String locationProvider = LocationManager.GPS_PROVIDER;
 
     EditText editText;
     ImageButton imageButton;
@@ -50,7 +59,10 @@ public class MainActivity extends AppCompatActivity {
     ImageView image_h;
     ImageView image_p;
     ImageView image_c;
+    ImageButton locationButton;
 
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,157 +90,139 @@ public class MainActivity extends AppCompatActivity {
         image_h = findViewById(R.id.image_h);
         image_p = findViewById(R.id.image_p);
         image_c = findViewById(R.id.image_c);
+        locationButton = findViewById(R.id.location);
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getWeather();
+                getWeatherByName();
+            }
+        });
+
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getWeatherByLocation();
             }
         });
     }
 
-    private void changeImgVisibility () {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getWeatherByLocation();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (locationManager != null) {
+            locationManager.removeUpdates(locationListener);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResult) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResult);
+
+        if (requestCode == REQUEST_CODE) {
+            if (grantResult.length > 0 && grantResult[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Got location successfully", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Can't get weather because you didn't provide access to the location", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void changeImgVisibility() {
         image_w.setVisibility(View.VISIBLE);
         image_h.setVisibility(View.VISIBLE);
         image_p.setVisibility(View.VISIBLE);
         image_c.setVisibility(View.VISIBLE);
     }
 
-    private void changeTextVisibility () {
+    private void changeTextVisibility() {
         wind1.setVisibility(View.VISIBLE);
         humidity1.setVisibility(View.VISIBLE);
         pressure1.setVisibility(View.VISIBLE);
         cloudiness1.setVisibility(View.VISIBLE);
     }
 
-    private void getCity(JSONObject jsonObject) throws JSONException {
-        String city_find = jsonObject.getString("name");
-        city.setText(city_find);
+    private  void updatedUI(WeatherData weatherData) {
+        changeImgVisibility();
+        changeTextVisibility();
+
+        city.setText(weatherData.getCity());
+        country.setText(weatherData.getCountry());
+        temperature.setText(Math.round(weatherData.getTemperature()) + "°");
+        feelsLike.setText("Feels like " + Math.round(weatherData.getFeelsLike()) + "°");
+        pressure.setText(weatherData.getPressure() + "hPa");
+        humidity.setText(weatherData.getHumidity() + "%");
+        Picasso.get().load(weatherData.getImg()).into(imageView);
+        description.setText(weatherData.getDescription());
+        wind.setText(weatherData.getWind() + "m/s");
+        cloudiness.setText(weatherData.getClouds() + "%");
+        MainActivity.this.date.setText(weatherData.getDate());
     }
 
-    private void getCountry(JSONObject jsonObject) throws JSONException {
-        JSONObject jsonObject1 = jsonObject.getJSONObject("sys");
+    private void getWeatherData(RequestParams requestParams) {
+        AsyncHttpClient client = new AsyncHttpClient();
 
-        String country_find = jsonObject1.getString("country");
-        country.setText(country_find);
-    }
-
-    private void getTemperature (JSONObject jsonObject) throws JSONException {
-        JSONObject jsonObject1 = jsonObject.getJSONObject("main");
-
-        String temperature_find = jsonObject1.getString("temp");
-        double temperature_c = Double.parseDouble(temperature_find) - 273.15;
-        temperature.setText( Math.round(temperature_c) + "°");
-    }
-
-    private void getFeelsLike (JSONObject jsonObject) throws JSONException {
-        JSONObject jsonObject1 = jsonObject.getJSONObject("main");
-
-        String feelsLike_find = jsonObject1.getString("feels_like");
-        double feelsLike_c = Double.parseDouble(feelsLike_find) - 273.15;
-        feelsLike.setText("Feels like " + Math.round(feelsLike_c) + "°");
-    }
-
-    private void getPressure (JSONObject jsonObject) throws JSONException {
-        JSONObject jsonObject1 = jsonObject.getJSONObject("main");
-
-        String pressure_find = jsonObject1.getString("pressure");
-        pressure.setText(pressure_find + "hPa");
-    }
-
-    private void getHumidity (JSONObject jsonObject) throws JSONException {
-        JSONObject jsonObject1 = jsonObject.getJSONObject("main");
-
-        String humidity_find = jsonObject1.getString("humidity");
-        humidity.setText(humidity_find + "%");
-    }
-
-    private void changeIcon (JSONObject jsonObject) throws JSONException {
-        JSONArray jsonArray = jsonObject.getJSONArray("weather");
-
-        JSONObject jsonObject1 = jsonArray.getJSONObject(0);
-        String img = jsonObject1.getString("icon");
-        Picasso.get().load("http://openweathermap.org/img/wn/" + img + "@2x.png").into(imageView);
-    }
-
-    private void getDescription (JSONObject jsonObject) throws JSONException {
-        JSONArray jsonArray = jsonObject.getJSONArray("weather");
-
-        JSONObject jsonObject1 = jsonArray.getJSONObject(0);
-
-        String description_find = jsonObject1.getString("description");
-        description.setText(description_find);
-    }
-
-    private void getWind (JSONObject jsonObject) throws JSONException {
-        JSONObject jsonObject1 = jsonObject.getJSONObject("wind");
-
-        String wind_find = jsonObject1.getString("speed");
-        wind.setText(wind_find + "m/s");
-    }
-
-    private void getClouds (JSONObject jsonObject) throws JSONException {
-        JSONObject jsonObject1 = jsonObject.getJSONObject("clouds");
-
-        String cloudiness_find = jsonObject1.getString("all");
-        cloudiness.setText(cloudiness_find + "%");
-    }
-
-    private void getDate (JSONObject jsonObject) throws JSONException {
-        String timezone_find = jsonObject.getString("timezone");
-
-        Calendar calendar = Calendar.getInstance();
-        Date date = new Date(calendar.getTimeInMillis());
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy \nHH:mm");
-        TimeZone my_time = TimeZone.getTimeZone("UTC");
-
-        my_time.setRawOffset(Integer.parseInt(timezone_find) * 1000);
-
-        simpleDateFormat.setTimeZone(my_time);
-        String formattedDate =  simpleDateFormat.format(date);
-        MainActivity.this.date.setText(formattedDate);
-    }
-
-    private void getWeather() {
-        String city_entered = editText.getText().toString();
-
-        String url = "http://api.openweathermap.org/data/2.5/weather?q="+city_entered+"&appid=33fe9ce138c6c6607762c5cecd8f9eb7";
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new  Response.Listener<String>() {
+        client.get(WEATHER_URL, requestParams, new JsonHttpResponseHandler() {
             @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                WeatherData weatherData = WeatherData.getAllData(response);
 
-                    changeTextVisibility();
-                    changeImgVisibility();
+                updatedUI(weatherData);
 
-                    changeIcon(jsonObject);
-
-                    getCity(jsonObject);
-                    getCountry(jsonObject);
-                    getTemperature(jsonObject);
-                    getFeelsLike(jsonObject);
-                    getPressure(jsonObject);
-                    getHumidity(jsonObject);
-                    getDescription(jsonObject);
-                    getWind(jsonObject);
-                    getClouds(jsonObject);
-                    getDate(jsonObject);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                Toast.makeText(MainActivity.this, "Got weather successfully", Toast.LENGTH_SHORT).show();
             }
-        }, new Response.ErrorListener() {
+
             @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, "There is no such location", Toast.LENGTH_SHORT).show();
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Toast.makeText(MainActivity.this, "Something went wrong: check internet connection or location you entered", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
-        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
-        requestQueue.add(stringRequest);
+    private void getWeatherByName() {
+        String city_entered = editText.getText().toString();
+
+        RequestParams requestParams = new RequestParams();
+        requestParams.put("q", city_entered);
+        requestParams.put("appid", API_KEY);
+
+        getWeatherData(requestParams);
+    }
+
+    private void getWeatherByLocation() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                String latitude = String.valueOf(location.getLatitude());
+                String longitude = String.valueOf(location.getLongitude());
+
+                RequestParams requestParams = new RequestParams();
+                requestParams.put("lat", latitude);
+                requestParams.put("lon", longitude);
+                requestParams.put("appid", API_KEY);
+
+                getWeatherData(requestParams);
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Toast.makeText(MainActivity.this, "Something went wrong. Can't get weather", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        locationManager.requestLocationUpdates(locationProvider, MIN_TIME, MIN_DISTANCE, locationListener);
     }
 }
