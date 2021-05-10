556966079e66939d2949b69d07ab7d30c235e7ce
AlexSemenenko <aleksei.syemyenenko@gmail.com>
2021-05-10 19:14:01 +0300

    added getters to class

--- app/src/main/java/com/example/weatherapp/WeatherData.java
+++ app/src/main/java/com/example/weatherapp/WeatherData.java
@@ -22,21 +22,65 @@ public class WeatherData {
     private String clouds;
     private String date;
 
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getFeelsLike() {
+        return feelsLike;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getWind() {
+        return wind;
+    }
+
+    public String getClouds() {
+        return clouds;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
     public static WeatherData getAllData(JSONObject jsonObject) {
         try {
             WeatherData weatherData = new WeatherData();
 
-            weatherData.city = getCity(jsonObject);
-            weatherData.country = getCounty(jsonObject);
-            weatherData.temperature = getTemperature(jsonObject);
-            weatherData.feelsLike = getFeelsLike(jsonObject);
-            weatherData.pressure = getPressure(jsonObject);
-            weatherData.humidity = getHumidity(jsonObject);
-            weatherData.img = getIcon(jsonObject);
-            weatherData.description = getDescription(jsonObject);
-            weatherData.wind = getWind(jsonObject);
-            weatherData.clouds = getClouds(jsonObject);
-            weatherData.date = getDate(jsonObject);
+            weatherData.city = getCityFromJson(jsonObject);
+            weatherData.country = getCountyFromJson(jsonObject);
+            weatherData.temperature = getTemperatureFromJson(jsonObject);
+            weatherData.feelsLike = getFeelsLikeFromJson(jsonObject);
+            weatherData.pressure = getPressureFromJson(jsonObject);
+            weatherData.humidity = getHumidityFromJson(jsonObject);
+            weatherData.img = getIconFromJson(jsonObject);
+            weatherData.description = getDescriptionFromJson(jsonObject);
+            weatherData.wind = getWindFromJson(jsonObject);
+            weatherData.clouds = getCloudsFromJson(jsonObject);
+            weatherData.date = getDateFromJson(jsonObject);
 
             return weatherData;
         } catch (JSONException e) {
@@ -45,47 +89,47 @@ public class WeatherData {
         }
     }
 
-    private static String getCity(JSONObject jsonObject)  throws JSONException {
+    private static String getCityFromJson(JSONObject jsonObject)  throws JSONException {
         return jsonObject.getString("name");
     }
 
-    private static String getCounty(JSONObject jsonObject)  throws JSONException {
+    private static String getCountyFromJson(JSONObject jsonObject)  throws JSONException {
         return jsonObject.getJSONObject("sys").getString("country");
     }
 
-    private static double getTemperature(JSONObject jsonObject)  throws JSONException {
+    private static double getTemperatureFromJson(JSONObject jsonObject)  throws JSONException {
         return Double.parseDouble(jsonObject.getJSONObject("main").getString("temp")) - 273.15;
     }
 
-    private static double getFeelsLike(JSONObject jsonObject)  throws JSONException {
+    private static double getFeelsLikeFromJson(JSONObject jsonObject)  throws JSONException {
         return Double.parseDouble(jsonObject.getJSONObject("main").getString("feels_like")) - 273.15;
     }
 
-    private static String getPressure(JSONObject jsonObject)  throws JSONException {
+    private static String getPressureFromJson(JSONObject jsonObject)  throws JSONException {
         return jsonObject.getJSONObject("main").getString("pressure");
     }
 
-    private static String getHumidity(JSONObject jsonObject)  throws JSONException {
+    private static String getHumidityFromJson(JSONObject jsonObject)  throws JSONException {
         return jsonObject.getJSONObject("main").getString("humidity");
     }
 
-    private static String getIcon(JSONObject jsonObject)  throws JSONException {
+    private static String getIconFromJson(JSONObject jsonObject)  throws JSONException {
         return "http://openweathermap.org/img/wn/" + jsonObject.getJSONArray("weather").getJSONObject(0).getString("icon") + "@2x.png";
     }
 
-    private static String getDescription(JSONObject jsonObject)  throws JSONException {
+    private static String getDescriptionFromJson(JSONObject jsonObject)  throws JSONException {
         return jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
     }
 
-    private static String getWind(JSONObject jsonObject)  throws JSONException {
+    private static String getWindFromJson(JSONObject jsonObject)  throws JSONException {
         return jsonObject.getJSONObject("wind").getString("speed");
     }
 
-    private static String getClouds(JSONObject jsonObject)  throws JSONException {
+    private static String getCloudsFromJson(JSONObject jsonObject)  throws JSONException {
         return jsonObject.getJSONObject("clouds").getString("all");
     }
 
-    private static String getDate(JSONObject jsonObject) throws JSONException {
+    private static String getDateFromJson(JSONObject jsonObject) throws JSONException {
         String timezone_find = jsonObject.getString("timezone");
 
         Calendar calendar = Calendar.getInstance();
